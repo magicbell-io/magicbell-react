e50d47120901a21cb351ee587cc3791d0c123a3e
Stephan Meijer <stephan.meijer@gmail.com>
2022-07-14 17:47:19 +0200

    chore: fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links',     '@storybook/addon-actions', '@storybook/addon-essentials'],
   staticDirs: ['../public'],
   babel: async (options) => {
     return {
